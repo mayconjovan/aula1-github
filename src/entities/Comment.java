c6208d546337071d571c72691effb857553cf704
Maycon J Pereira <mayconjovan@gmail.com>
2020-02-14 01:15:44 -0300

    StringBuilder

---
+++ src/entities/Comment.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+/**
+ *
+ * @author Deivinson
+ */
+public class Comment {
+    private String text;
+    
+    public Comment(){
+        
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+    
+    
+}
