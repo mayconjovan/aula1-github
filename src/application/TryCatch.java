cd8564401fb43110d048650cce846fffd4adc490
Maycon J Pereira <mayconjovan@gmail.com>
2020-02-22 00:54:29 -0300

    MultiCatch

--- src/application/TryCatch.java
+++ src/application/TryCatch.java
@@ -15,21 +15,33 @@ import java.util.Scanner;
 public class TryCatch {
 
     public static void main(String[] args) {
+        method1();
+        System.out.println("End of programa");
+    }
+    
+    public static void method1(){
+        System.out.println("METODO 1");
+        method2();
+        System.out.println("fim metodo 1");
+    }
+    
+    public static void method2() {
+        System.out.println("***METODO 2 STRAT****");
         Scanner sc = new Scanner(System.in);
 
         String[] vect = sc.nextLine().split(" ");
         try {
             int position = sc.nextInt();
             System.out.println(vect[position]);
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position!");
-        }
-        catch (InputMismatchException e) {
+            e.printStackTrace();
+            sc.next();
+        } catch (InputMismatchException e) {
             System.out.println("Input error");
         }
-        System.out.println("End of programa");
         sc.close();
+        System.out.println("***METHODO 2 END***");
     }
 
 }
